279c8e283741b0066f621f3ebb31e8b1c5d88588
NamanSaxena3108 <naman2004saxena@gmail.com>
2024-09-10 20:07:14 +0530

    Final Updates

--- dashboard.py
+++ dashboard.py
@@ -28,7 +28,7 @@ def main():
     st.image("hero_image1.jpg",use_column_width=True)
     with st.spinner("Loading Pokemon..."):
         df=load_data()
-        st.snow()
+
     rows,columns=df.shape
     col_name=df.columns.tolist()
 
@@ -37,6 +37,51 @@ def main():
     c2.subheader(f"Total Column: {columns}")
     c3.subheader(f"Columns: {",".join(col_name)}")
     
+    c1.metric("Pokemon Power",df.Total.sum(),delta=df.Total.mean())
+
+    count=df["Type 1"].value_counts()
+    fig, ax = plt.subplots(figsize=(10,5))
+    sns.barplot(x=count.index,y=count,ax=ax)
+    plt.xticks(rotation=90)
+    c3.pyplot(fig)
+
+    count2=df['Type 2'].value_counts()
+    fig2 =px.bar(count2,count2.index,count2.values)
+    c2.plotly_chart(fig2)
+    
+    cols=["HP","Attack","Defense","Sp. Atk","Sp. Def","Speed"]
+    c1.subheader("Pokemon Stats")
+    selection=c1.multiselect("Select Stats",cols,default=['HP',"Attack"])
+
+    if selection:
+        fig3=px.scatter_matrix(df,dimensions=selection,
+                               color="Generation",height=800)
+        st.plotly_chart(fig3,use_container_width=True)
+
+        fig4=px.line(df,y=selection,color="Generation",height=800)
+        st.plotly_chart(fig4,use_container_width=True)
+
+        fig5=px.histogram(df,x=selection,height=800)
+        st.plotly_chart(fig5,use_container_width=True)
+
+    c1,c2,c3,c4=st.columns(4)
+    num_cols=df.select_dtypes(include=[np.number]).columns.tolist()
+    cat_cols=df.select_dtypes(exclude=[np.number]).columns.tolist()[1:]
+    x=c1.selectbox("Select X-axis",num_cols,index=0)
+    y=c2.selectbox("Select Y-axis",num_cols,index=1)
+    z=c3.selectbox("Select hue",num_cols,index=2)
+    face=c4.selectbox("Select Face",cat_cols)
+
+    fig6=px.scatter(df,x=x,y=y,color=z,size="Total",
+                    hover_name="Name",
+                    facet_col=face,
+                    height=1000,
+                    facet_col_wrap=3)
+    st.plotly_chart(fig6,use_container_width=True)
+
+    fig7=px.scatter_3d(df,x=x,y=y,z=z,color="Generation",size="Total",
+                       hover_name="Name",height=800)
+    st.plotly_chart(fig7,use_container_width=True)
 
 if __name__=="__main__":
     main()
